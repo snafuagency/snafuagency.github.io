adcc66281d086c663eed502551c827b588777502
snafuagency <snafuagency@users.noreply.github.com>
2020-11-06 19:21:07 -0800

    overhaul to mouse tracking

--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,13 @@
 
+// credit start https://github.com/ephraimilunga/fylo_challenge/blob/master/js/main.js
+const mouse = document.querySelector('.crosshair');
+const hover = "hover";
+function moveMouse(e){
+    const x = e.clientX;
+    const y = e.clientY;
+    mouse.style.top = 0; // added by Ian Quinn
+    mouse.style.left = 0; // added by Ian Quinn
+    mouse.style.transform = `translate(${x - 37}px, ${y - 39}px)`;
+};
+document.addEventListener("mousemove", moveMouse)
+// credit end
