0cd71e589e0e5fd0df27b9cfac2162b5813dac5d
snafuagency <snafuagency@users.noreply.github.com>
2021-02-19 15:16:18 -0800

    design sprint
    many styles and semantics updated

--- js/theme.js
+++ js/theme.js
@@ -1,14 +1,12 @@
 // © 2021, Ian Quinn
 
-// credit start https://github.com/ephraimilunga/fylo_challenge/blob/master/js/main.js
+// inspired by https://github.com/ephraimilunga/fylo_challenge/blob/master/js/main.js
 const mouse = document.querySelector('.crosshair');
 const hover = "hover";
 function moveMouse(e){
     const x = e.clientX;
     const y = e.clientY;
-    mouse.style.top = 0; // added by Ian Quinn
-    mouse.style.left = 0; // added by Ian Quinn
-    mouse.style.transform = `translate(${x - 37}px, ${y - 39}px)`; // values modified by Ian Quinn
+    mouse.style.top = (y - 39) + 'px';
+    mouse.style.left = (x - 37) + 'px';
 };
 document.addEventListener("mousemove", moveMouse) 
-// credit end
