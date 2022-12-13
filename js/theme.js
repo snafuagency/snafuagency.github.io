04806f02ad4b0042f6eef073e6adfab06ea5d4e1
snafuagency <snafuagency@users.noreply.github.com>
2022-12-12 21:53:25 -0800

    style updates

--- js/theme.js
+++ js/theme.js
@@ -10,63 +10,8 @@ function moveMouse(e){
     mouse.style.left = (x - 24) + 'px';
 };
 document.addEventListener("mousemove", moveMouse, {passive: true})
-// import * as THREE from '../node_modules/three/build/three.module.js';
-// import { OBJLoader } from '../node_modules/three/examples/jsm/loaders/OBJLoader.js';
 
-// // Create scene
-// const scene = new THREE.Scene();
-// const camera = new THREE.PerspectiveCamera( 75, 1 / 1, 0.1, 1000 );
-// const renderer = new THREE.WebGLRenderer({ alpha: true } );
-// renderer.setSize( 400, 400 );
-// renderer.setClearColor( 0x000000, 0 );
-// document.body.appendChild( renderer.domElement );
+// Map Viewer
+const model_dimensions = document.querySelector('model-viewer').getDimensions();
 
-
-// // instantiate a loader
-// const loader = new OBJLoader();
-
-// // load a resource
-// loader.load(
-// 	// resource URL
-// 	'../models/ian.vox.obj',
-// 	// called when resource is loaded
-// 	function ( object ) {
-
-// 		scene.add( object );
-
-// 	},
-// 	// called when loading is in progresses
-// 	function ( xhr ) {
-
-// 		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-// 	},
-// 	// called when loading has errors
-// 	function ( error ) {
-
-// 		console.log( 'An error happened' );
-
-// 	}
-// );
-
-
-// // Load geometry
-// const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0x7B1FA2 } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-
-
-
-
-
-
-// camera.position.z = 2;
-
-// // Render Scene
-// function animate() {
-// 	requestAnimationFrame( animate );
-// 	cube.rotation.y += 0.01;
-// 	renderer.render( scene, camera );
-// }
-// animate();
+console.log(model_dimensions);
