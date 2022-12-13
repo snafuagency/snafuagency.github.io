bb08a397d6ded6ef95a6b3b68fa298c9d5e96b2b
snafuagency <snafuagency@users.noreply.github.com>
2022-12-13 02:10:11 -0800

    stats added

--- js/theme.js
+++ js/theme.js
@@ -2,16 +2,19 @@
 
 // inspired by https://github.com/ephraimilunga/fylo_challenge/blob/master/js/main.js
 const mouse = document.querySelector('.crosshair');
+const mouse_x_coord = document.querySelector('.mouse-coordinates__x');
+const mouse_y_coord = document.querySelector('.mouse-coordinates__y');
 const hover = "hover";
 function moveMouse(e){
     const x = e.clientX;
     const y = e.clientY;
     mouse.style.top = (y - 24) + 'px';
     mouse.style.left = (x - 24) + 'px';
+	mouse_x_coord.textContent = x;
+	mouse_y_coord.textContent = y;
 };
 document.addEventListener("mousemove", moveMouse, {passive: true})
 
-// Map Viewer
-const model_dimensions = document.querySelector('model-viewer').getDimensions();
 
-console.log(model_dimensions);
+const clock_num = document.querySelector('.clock__num');
+setInterval(() => clock_num.textContent = new Date(),1000);
