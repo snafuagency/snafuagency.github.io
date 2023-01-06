dcd3d495b106e189c77c819dc48a007941d0b4cd
snafuagency <snafuagency@users.noreply.github.com>
2023-01-06 15:46:40 -0800

    new corner crosshair reticles

--- js/theme.js
+++ js/theme.js
@@ -1,15 +1,15 @@
 // © 2021, Ian Quinn
 
 // Mouse Cusor inspired by https://github.com/ephraimilunga/fylo_challenge/blob/master/js/main.js
-const mouse = document.querySelector('.crosshair');
+const mouse = document.querySelector('.crosshair--mouse');
 const mouse_x_coord = document.querySelector('.mouse-coordinates__x');
 const mouse_y_coord = document.querySelector('.mouse-coordinates__y');
 const hover = "hover";
 function moveMouse(e){
     const x = e.clientX;
     const y = e.clientY;
-    mouse.style.top = (y - 28) + 'px';
-    mouse.style.left = (x - 26) + 'px';
+    mouse.style.top = (y - 36) + 'px';
+    mouse.style.left = (x - 35) + 'px';
 	mouse_x_coord.textContent = x;
 	mouse_y_coord.textContent = y;
 };
