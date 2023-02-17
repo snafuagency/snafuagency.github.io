cd414bf1bf342cce8dca23fbbd3cbbc21b1a00e8
snafuagency <snafuagency@users.noreply.github.com>
2023-02-17 13:44:16 -0800

    animation update

--- js/theme.js
+++ js/theme.js
@@ -18,6 +18,22 @@ const copyright_options = {
 }
 copyright_year.textContent = new Date().toLocaleDateString("en-CA", copyright_options);
 
+
+// Reveal on scroll inspired by  https://www.youtube.com/watch?v=T33NN_pPeNI
+
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		// console.log(entry)
+		if (entry.isIntersecting){
+			entry.target.classList.add('reveal');
+		} //else {
+			//entry.target.classList.remove('reveal'); 
+		//}
+	});
+});
+const revealOnScroll = document.querySelectorAll('.reveal-on-scroll');
+revealOnScroll.forEach((el) => observer.observe(el));  
+
 // Mouse Cusor inspired by https://github.com/ephraimilunga/fylo_challenge/blob/master/js/main.js
 // const mouse = document.querySelector('.crosshair--mouse');
 // const mouse_x_coord = document.querySelector('.mouse-coordinates__x');
@@ -34,17 +50,17 @@ copyright_year.textContent = new Date().toLocaleDateString("en-CA", copyright_op
 // document.addEventListener("mousemove", moveMouse, {passive: true})
 
 // Clock
-const clock_num = document.querySelector('.clock__num');
-const clock_options = { 
-	weekday: 'narrow', 
-	month: 'narrow', 
-	day: '2-digit',
-	year: '2-digit',
-	hour12: false,
-	hour: '2-digit',
-	minute: '2-digit',
-	second: '2-digit',
-	fractionalSecondDigits: 2,	
-	timeZoneName: "shortOffset"
-}
-setInterval(() => clock_num.textContent = new Date().toLocaleDateString("en-CA", clock_options), 10);
+// const clock_num = document.querySelector('.clock__num');
+// const clock_options = { 
+// 	weekday: 'narrow', 
+// 	month: 'narrow', 
+// 	day: '2-digit',
+// 	year: '2-digit',
+// 	hour12: false,
+// 	hour: '2-digit',
+// 	minute: '2-digit',
+// 	second: '2-digit',
+// 	fractionalSecondDigits: 2,	
+// 	timeZoneName: "shortOffset"
+// }
+// setInterval(() => clock_num.textContent = new Date().toLocaleDateString("en-CA", clock_options), 10);
