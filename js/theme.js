620fbd881fcea26557180078425128d0365afbbf
snafuagency <snafuagency@users.noreply.github.com>
2023-05-28 23:35:04 -0700

    styling fixes

--- js/theme.js
+++ js/theme.js
@@ -92,32 +92,32 @@ function hacked_text_loop() {
 setInterval(hacked_text_loop, 4000);
 
 // Mouse Cusor inspired by https://github.com/ephraimilunga/fylo_challenge/blob/master/js/main.js
-//const mouse = document.querySelector('.crosshair--mouse');
-// const mouse_x_coord = document.querySelector('.mouse-coordinates__x');
-// const mouse_y_coord = document.querySelector('.mouse-coordinates__y');
-// const hover = "hover";
-// function moveMouse(e){
-//     const x = e.clientX;
-//     const y = e.clientY;
-//     //mouse.style.top = (y - 36) + 'px';
-//     //mouse.style.left = (x - 35) + 'px';
-// 	mouse_x_coord.textContent = x;
-// 	mouse_y_coord.textContent = y;
-// };
-// document.addEventListener("mousemove", moveMouse, {passive: true}) 
+const mouse = document.querySelector('.crosshair--mouse');
+const mouse_x_coord = document.querySelector('.mouse-coordinates__x');
+const mouse_y_coord = document.querySelector('.mouse-coordinates__y');
+const hover = "hover";
+function moveMouse(e){
+    const x = e.clientX;
+    const y = e.clientY;
+    //mouse.style.top = (y - 36) + 'px';
+    //mouse.style.left = (x - 35) + 'px';
+	mouse_x_coord.textContent = x;
+	mouse_y_coord.textContent = y;
+};
+document.addEventListener("mousemove", moveMouse, {passive: true}) 
 
 // Clock
-// const clock_num = document.querySelector('.clock__num');
-// const clock_options = { 
-// 	weekday: 'narrow', 
-// 	month: 'narrow', 
-// 	day: '2-digit',
-// 	year: '2-digit',
-// 	hour12: false,
-// 	hour: '2-digit',
-// 	minute: '2-digit',
-// 	second: '2-digit',
-// 	fractionalSecondDigits: 2,	
-// 	timeZoneName: "shortOffset"
-// }
-// setInterval(() => clock_num.textContent = new Date().toLocaleDateString("en-CA", clock_options), 10);
+const clock_num = document.querySelector('.clock__num');
+const clock_options = { 
+	weekday: 'narrow', 
+	month: 'narrow', 
+	day: '2-digit',
+	year: '2-digit',
+	hour12: false,
+	hour: '2-digit',
+	minute: '2-digit',
+	second: '2-digit',
+	fractionalSecondDigits: 2,	
+	timeZoneName: "shortOffset"
+}
+setInterval(() => clock_num.textContent = new Date().toLocaleDateString("en-CA", clock_options), 10);
